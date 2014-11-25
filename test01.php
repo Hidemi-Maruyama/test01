5f64b7b599bc0c26cfb59035443889406a8565d7
Hidemi-Maruyama <acusmata@icloud.com>
2014-11-25 17:59:00 +0900

    Initial commit

---
+++ test01.php
@@ -0,0 +1,20 @@
+<html>
+<meta charset="utf-8">
+<body>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: maruyama
+ * Date: 2014/10/21
+ * Time: 13:49
+ */
+    echo "<h1>これは PHP のテストです。</h1>";
+
+    var_dump($_SERVER);
+
+    var_dump(get_defined_constants());
+
+?>
+</body>
+</html>
+
